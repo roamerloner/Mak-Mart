f58aeff6a815dc855403cd624603f952a358c798
roamerloner <mkhan181046@bscse.uiu.ac.bd>
2024-10-11 04:09:17 +0600

    Login page completed

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
  const Register = () => {
   return (
@@ -33,6 +35,25 @@ import { Link } from 'react-router-dom';
               <div className='flex items-center mb-3 gap-3 justify-center'>
                 <p>Already have account? <Link className='font-bold' to='/login'>Sign In</Link></p>
               </div>
+
+              <div className='w-full flex justify-center items-center mb-3'>
+                <div className='w-[45%] bg-slate-700 h-[1px]'></div>
+                  <div className='w-[10%] flex justify-center items-center'>
+                    <span className='pb-1'>Or</span>
+                  </div>
+                  <div className='w-[45%] bg-slate-700 h-[1px]'></div>
+              </div>
+
+              <div className='flex justify-center items-center gap-3'>
+                <div className='w-[135px] h-[35px] flex rounded-md bg-orange-700 shadow-lg hover:shadow-orange-700/50 justify-center cursor-pointer items-center overflow-hidden'>
+                  <span><FaGoogle /></span>
+                </div>
+
+                <div className='w-[135px] h-[35px] flex rounded-md bg-blue-700 shadow-lg hover:shadow-blue-700/50 justify-center cursor-pointer items-center overflow-hidden'>
+                  <span><FaFacebook /></span>
+                </div>
+              </div>
+
             </form>
 
           </div>
